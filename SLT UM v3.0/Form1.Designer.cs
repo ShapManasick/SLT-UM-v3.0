a4a7019b24652dfc029e6a08ceb22533cd8bb049
Harith Dilshan <harithdilshan777@gmail.com>
2020-02-11 15:37:04 +0530

    Add files via upload

---
+++ SLT UM v3.0/Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace SLT_UM_v3._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SussBox = new FlatUI.FlatAlertBox();
+            this.ErrorBox = new FlatUI.FlatAlertBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.flatButton2 = new FlatUI.FlatButton();
+            this.flatButton1 = new FlatUI.FlatButton();
+            this.flatLabel7 = new FlatUI.FlatLabel();
+            this.flatLabel6 = new FlatUI.FlatLabel();
+            this.flatLabel5 = new FlatUI.FlatLabel();
+            this.txtPassword = new FlatUI.FlatTextBox();
+            this.flatLabel4 = new FlatUI.FlatLabel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.flatLabel2 = new FlatUI.FlatLabel();
+            this.txtUName = new FlatUI.FlatTextBox();
+            this.flatLabel3 = new FlatUI.FlatLabel();
+            this.flatLabel1 = new FlatUI.FlatLabel();
+            this.txtLable = new FlatUI.FlatTextBox();
+            this.formSkin1 = new FlatUI.FormSkin();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.formSkin1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SussBox
+            // 
+            this.SussBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(70)))), ((int)(((byte)(73)))));
+            this.SussBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SussBox.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.SussBox.kind = FlatUI.FlatAlertBox._Kind.Success;
+            this.SussBox.Location = new System.Drawing.Point(-1, 305);
+            this.SussBox.Name = "SussBox";
+            this.SussBox.Size = new System.Drawing.Size(600, 42);
+            this.SussBox.TabIndex = 20;
+            this.SussBox.Text = "Saved successfully !";
+            this.SussBox.Visible = false;
+            // 
+            // ErrorBox
+            // 
+            this.ErrorBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(70)))), ((int)(((byte)(73)))));
+            this.ErrorBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ErrorBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ErrorBox.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.ErrorBox.kind = FlatUI.FlatAlertBox._Kind.Error;
+            this.ErrorBox.Location = new System.Drawing.Point(0, 305);
+            this.ErrorBox.Name = "ErrorBox";
+            this.ErrorBox.Size = new System.Drawing.Size(599, 42);
+            this.ErrorBox.TabIndex = 0;
+            this.ErrorBox.Text = "Please fill all fields...!";
+            this.ErrorBox.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(137)))), ((int)(((byte)(117)))));
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(550, 10);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(33, 31);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 19;
+            this.pictureBox2.TabStop = false;
+            // 
+            // flatButton2
+            // 
+            this.flatButton2.BackColor = System.Drawing.Color.Transparent;
+            this.flatButton2.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
+            this.flatButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.flatButton2.Font = new System.Drawing.Font("Lato Light", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flatButton2.Location = new System.Drawing.Point(380, 263);
+            this.flatButton2.Name = "flatButton2";
+            this.flatButton2.Rounded = false;
+            this.flatButton2.Size = new System.Drawing.Size(174, 44);
+            this.flatButton2.TabIndex = 18;
+            this.flatButton2.Text = "Next";
+            this.flatButton2.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            // 
+            // flatButton1
+            // 
+            this.flatButton1.BackColor = System.Drawing.Color.Transparent;
+            this.flatButton1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
+            this.flatButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.flatButton1.Font = new System.Drawing.Font("Lato Light", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flatButton1.Location = new System.Drawing.Point(213, 263);
+            this.flatButton1.Name = "flatButton1";
+            this.flatButton1.Rounded = false;
+            this.flatButton1.Size = new System.Drawing.Size(161, 44);
+            this.flatButton1.TabIndex = 17;
+            this.flatButton1.Text = "Save";
+            this.flatButton1.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(243)))), ((int)(((byte)(243)))));
+            // 
+            // flatLabel7
+            // 
+            this.flatLabel7.AutoSize = true;
+            this.flatLabel7.BackColor = System.Drawing.Color.Transparent;
+            this.flatLabel7.Font = new System.Drawing.Font("Lato Light", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flatLabel7.ForeColor = System.Drawing.Color.White;
+            this.flatLabel7.Location = new System.Drawing.Point(192, 215);
+            this.flatLabel7.Name = "flatLabel7";
+            this.flatLabel7.Size = new System.Drawing.Size(19, 29);
+            this.flatLabel7.TabIndex = 16;
+            this.flatLabel7.Text = ":";
+            // 
+            // flatLabel6
+            // 
+            this.flatLabel6.AutoSize = true;
+            this.flatLabel6.BackColor = System.Drawing.Color.Transparent;
+            this.flatLabel6.Font = new System.Drawing.Font("Lato Light", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flatLabel6.ForeColor = System.Drawing.Color.White;
+            this.flatLabel6.Location = new System.Drawing.Point(192, 171);
+            this.flatLabel6.Name = "flatLabel6";
+            this.flatLabel6.Size = new System.Drawing.Size(19, 29);
+            this.flatLabel6.TabIndex = 15;
+            this.flatLabel6.Text = ":";
+            // 
+            // flatLabel5
+            // 
+            this.flatLabel5.AutoSize = true;
+            this.flatLabel5.BackColor = System.Drawing.Color.Transparent;
+            this.flatLabel5.Font = new System.Drawing.Font("Lato Light", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flatLabel5.ForeColor = System.Drawing.Color.White;
+            this.flatLabel5.Location = new System.Drawing.Point(192, 126);
+            this.flatLabel5.Name = "flatLabel5";
+            this.flatLabel5.Size = new System.Drawing.Size(19, 29);
+            this.flatLabel5.TabIndex = 14;
+            this.flatLabel5.Text = ":";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.Color.Transparent;
+            this.txtPassword.FocusOnHover = false;
+            this.txtPassword.Location = new System.Drawing.Point(213, 215);
+            this.txtPassword.MaxLength = 32767;
+            this.txtPassword.Multiline = false;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.ReadOnly = false;
+            this.txtPassword.Size = new System.Drawing.Size(341, 34);
+            this.txtPassword.TabIndex = 13;
+            this.txtPassword.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txtPassword.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.txtPassword.UseSystemPasswordChar = false;
+            // 
+            // flatLabel4
+            // 
+            this.flatLabel4.AutoSize = true;
+            this.flatLabel4.BackColor = System.Drawing.Color.Transparent;
+            this.flatLabel4.Font = new System.Drawing.Font("Lato Light", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flatLabel4.ForeColor = System.Drawing.Color.White;
+            this.flatLabel4.Location = new System.Drawing.Point(34, 131);
+            this.flatLabel4.Name = "flatLabel4";
+            this.flatLabel4.Size = new System.Drawing.Size(68, 29);
+            this.flatLabel4.TabIndex = 12;
+            this.flatLabel4.Text = "Lable";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.flatLabel2);
+            this.panel1.Location = new System.Drawing.Point(-1, 49);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(594, 57);
+            this.panel1.TabIndex = 11;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(155, 7);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(53, 45);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // flatLabel2
+            // 
+            this.flatLabel2.AutoSize = true;
+            this.flatLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.flatLabel2.Font = new System.Drawing.Font("Lato Light", 19.8F);
+            this.flatLabel2.ForeColor = System.Drawing.Color.White;
+            this.flatLabel2.Location = new System.Drawing.Point(207, 10);
+            this.flatLabel2.Name = "flatLabel2";
+            this.flatLabel2.Size = new System.Drawing.Size(219, 40);
+            this.flatLabel2.TabIndex = 10;
+            this.flatLabel2.Text = "Create Profile ";
+            // 
+            // txtUName
+            // 
+            this.txtUName.BackColor = System.Drawing.Color.Transparent;
+            this.txtUName.FocusOnHover = false;
+            this.txtUName.Location = new System.Drawing.Point(213, 171);
+            this.txtUName.MaxLength = 32767;
+            this.txtUName.Multiline = false;
+            this.txtUName.Name = "txtUName";
+            this.txtUName.ReadOnly = false;
+            this.txtUName.Size = new System.Drawing.Size(341, 34);
+            this.txtUName.TabIndex = 9;
+            this.txtUName.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txtUName.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.txtUName.UseSystemPasswordChar = false;
+            // 
+            // flatLabel3
+            // 
+            this.flatLabel3.AutoSize = true;
+            this.flatLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.flatLabel3.Font = new System.Drawing.Font("Lato Light", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flatLabel3.ForeColor = System.Drawing.Color.White;
+            this.flatLabel3.Location = new System.Drawing.Point(34, 220);
+            this.flatLabel3.Name = "flatLabel3";
+            this.flatLabel3.Size = new System.Drawing.Size(117, 29);
+            this.flatLabel3.TabIndex = 8;
+            this.flatLabel3.Text = "Password ";
+            // 
+            // flatLabel1
+            // 
+            this.flatLabel1.AutoSize = true;
+            this.flatLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.flatLabel1.Font = new System.Drawing.Font("Lato Light", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.flatLabel1.ForeColor = System.Drawing.Color.White;
+            this.flatLabel1.Location = new System.Drawing.Point(34, 176);
+            this.flatLabel1.Name = "flatLabel1";
+            this.flatLabel1.Size = new System.Drawing.Size(117, 29);
+            this.flatLabel1.TabIndex = 7;
+            this.flatLabel1.Text = "Username";
+            // 
+            // txtLable
+            // 
+            this.txtLable.BackColor = System.Drawing.Color.Transparent;
+            this.txtLable.FocusOnHover = false;
+            this.txtLable.Location = new System.Drawing.Point(213, 126);
+            this.txtLable.MaxLength = 32767;
+            this.txtLable.Multiline = false;
+            this.txtLable.Name = "txtLable";
+            this.txtLable.ReadOnly = false;
+            this.txtLable.Size = new System.Drawing.Size(341, 34);
+            this.txtLable.TabIndex = 6;
+            this.txtLable.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txtLable.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.txtLable.UseSystemPasswordChar = false;
+            // 
+            // formSkin1
+            // 
+            this.formSkin1.BackColor = System.Drawing.Color.White;
+            this.formSkin1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(68)))), ((int)(((byte)(85)))));
+            this.formSkin1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(36)))), ((int)(((byte)(42)))));
+            this.formSkin1.Controls.Add(this.SussBox);
+            this.formSkin1.Controls.Add(this.ErrorBox);
+            this.formSkin1.Controls.Add(this.pictureBox2);
+            this.formSkin1.Controls.Add(this.flatButton2);
+            this.formSkin1.Controls.Add(this.flatButton1);
+            this.formSkin1.Controls.Add(this.flatLabel7);
+            this.formSkin1.Controls.Add(this.flatLabel6);
+            this.formSkin1.Controls.Add(this.flatLabel5);
+            this.formSkin1.Controls.Add(this.txtPassword);
+            this.formSkin1.Controls.Add(this.flatLabel4);
+            this.formSkin1.Controls.Add(this.panel1);
+            this.formSkin1.Controls.Add(this.txtUName);
+            this.formSkin1.Controls.Add(this.flatLabel3);
+            this.formSkin1.Controls.Add(this.flatLabel1);
+            this.formSkin1.Controls.Add(this.txtLable);
+            this.formSkin1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.formSkin1.FlatColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(168)))), ((int)(((byte)(109)))));
+            this.formSkin1.Font = new System.Drawing.Font("Lato Light", 13.8F);
+            this.formSkin1.HeaderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(137)))), ((int)(((byte)(117)))));
+            this.formSkin1.HeaderMaximize = false;
+            this.formSkin1.Location = new System.Drawing.Point(0, 0);
+            this.formSkin1.Name = "formSkin1";
+            this.formSkin1.Size = new System.Drawing.Size(599, 347);
+            this.formSkin1.TabIndex = 1;
+            this.formSkin1.Text = "SLT UM v3.0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 347);
+            this.Controls.Add(this.formSkin1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Fuchsia;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.formSkin1.ResumeLayout(false);
+            this.formSkin1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FlatUI.FlatAlertBox SussBox;
+        private FlatUI.FlatAlertBox ErrorBox;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private FlatUI.FlatButton flatButton2;
+        private FlatUI.FlatButton flatButton1;
+        private FlatUI.FlatLabel flatLabel7;
+        private FlatUI.FlatLabel flatLabel6;
+        private FlatUI.FlatLabel flatLabel5;
+        private FlatUI.FlatTextBox txtPassword;
+        private FlatUI.FlatLabel flatLabel4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private FlatUI.FlatLabel flatLabel2;
+        private FlatUI.FlatTextBox txtUName;
+        private FlatUI.FlatLabel flatLabel3;
+        private FlatUI.FlatLabel flatLabel1;
+        private FlatUI.FlatTextBox txtLable;
+        private FlatUI.FormSkin formSkin1;
+    }
+}
+
